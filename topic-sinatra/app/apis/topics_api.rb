04719774d457527501e5abee5bbd495517008f6f
wurui <inosin7000@gmail.com>
2013-05-22 17:35:32 +0800

    add sinatra app

---
+++ topic-sinatra/app/apis/topics_api.rb
@@ -0,0 +1,14 @@
+class TopicsAPI < Sinatra::Base
+  helpers Sinatra::JSON
+
+  get '/new_topics.json' do
+    @topics = Topic.last(10)
+    json @topics
+  end
+
+  get '/topic/:topic_id/users.json' do
+    @topic = Topic.find params[:topic_id]
+    post = @topic.posts.includes(:user).map(&:user)
+    json post
+  end
+end
